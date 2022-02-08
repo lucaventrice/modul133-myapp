03e2b764e9896b139d1b03b28a3a8326e9518ed5
Luca Ventrice <lve127262@iet-gibb.ch>
2022-02-08 09:50:48 +0100

    m133 teil 2: example website

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def texte
+  end
+
+  def bilder
+  end
+
+  def formulare
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # get 'pages/home'
   root 'pages#home'
+  get '/pages/texte'
+  get '/pages/bilder'
+  get '/pages/formulare'
   get 'helloworld', to: 'helloworld#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
